6890ad70abce20546bf0276d8e917fd34d10ef36
don <don.tinimbang@gmail.com>
2018-04-20 23:51:41 -0500

    still no luck

--- reactpractice/src/App.js
+++ reactpractice/src/App.js
@@ -20,16 +20,7 @@ class App extends Component {
         return (
             <Wrapper>
                 <Title>This is me, MARIO!</Title>
-        
-                <h3 className="scoreSummary">
-                    {this.state.clickMessage}
-                </h3>
-                
-                <h3 className="scoreSummary">
-                    Correct Guesses: {this.state.correctGuesses} 
-                    <br />
-                    Best Score: {this.state.bestScore} 
-                </h3>
+     
 
                 {this.state.matches.map(match => (
                     <MatchCard
